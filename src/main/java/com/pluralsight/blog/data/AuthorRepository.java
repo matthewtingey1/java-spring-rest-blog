6e663587906510883c6c803bcb4ff65196786289
matthewtingey1 <matthewtingey1@mail.weber.edu>
2021-08-15 12:43:16 -0600

    second module completed Matt Tingey

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,5 +2,7 @@ package com.pluralsight.blog.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.pluralsight.blog.model.Author;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {}
